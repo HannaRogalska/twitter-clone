e198fc7328b151a57bc3c325c2d720dc46883baa
Hanna Rogalska <hanrogalska@gmail.com>
2024-12-10 12:43:04 +0100

    1 - auth controllers added

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { signup, login, logout } from "../controllers/auth.controllers.js"; 
+import { signup, login, logout, getMe } from "../controllers/auth.controllers.js"; 
+import { protectRoute } from "../middleware/protectRoute.js";
 
 const route = express.Router();
 
-route.get("/signup", signup);
-
-route.post("login", login);
-route.post("logout", logout);
+route.get("/me", protectRoute, getMe);
+route.post("/signup", signup);
+route.post("/login", login);
+route.post("/logout", logout);
 
 export default route;
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,21 @@
-import express from 'express';
+import express, { json } from 'express';
 import authRotes from './routes/auth.routes.js'
 import dotenv from 'dotenv';
 import { connectMongoDB } from './db/connectMongoDB.js';
+import cookieParser from "cookie-parser";
+
+
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/auth", authRotes);
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT} `);
