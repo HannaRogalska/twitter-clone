8b2d35d5873c158b2b5d298b0ced0c56e698662f
Hanna Rogalska <hanrogalska@gmail.com>
2025-01-09 10:42:48 +0100

    3 - Auth handled and routes protected in client

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -56,7 +56,10 @@ export const login = async (req, res) => {
   try {
     const { username, password } = req.body;
     const user = await User.findOne({ username });
-    const isPasswordCorrect = bcrypt.compare(password, user?.password || "");
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
     if (!user || !isPasswordCorrect) {
       return res.status(400).json({ error: "Invalid username or password" });
     }
--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
