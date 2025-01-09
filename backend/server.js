0778d20ecd03cf85b4b537074363f60328fa25c5
Hanna Rogalska <hanrogalska@gmail.com>
2025-01-09 17:27:28 +0100

    3 - payload limit updated in server.js

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,9 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
